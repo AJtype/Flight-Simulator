e6a3500b85ce7a131d4d26786b48efb390982239
Amit Judes <amit@judes.co.il>
2025-10-20 17:16:37 +0300

    changed folder drone to UAV and added folder utils currently it only hold PI

--- UAV/UAV.hpp
+++ UAV/UAV.hpp
@@ -4,13 +4,20 @@
 
 class UAV { // TODO
 private:
+    size_t id;
+    
     double curr_x;
     double curr_y;
     double curr_z;
-    size_t id;
+
     double target_x;
     double target_y;
-    double azimuth;
+    double target_z; // currently doesn't change
+
+    double azimuth; // TODO: in what format is this saved?
+    double minRadius; // currently doesn't change
+    double velocity; // currently doesn't change
     bool isCircling;
+
     std::ofstream outFile;
 };
